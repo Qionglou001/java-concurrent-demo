cd05307503a05ef6d10e879d7cf787ad16dfc9df
Qionglou001 <Qionglou001@users.noreply.github.com>
2018-11-12 13:48:41 +0800

    [CHG]修改一些代码描述。。。。

--- src/cuncorrent/countdownlatch/CountDownLatchTest.java
+++ src/cuncorrent/countdownlatch/CountDownLatchTest.java
@@ -5,7 +5,7 @@ import java.util.concurrent.CountDownLatch;
 /**
  * CountDownLatch是一个同步工具类，它允许一个或多个线程一直等待，直到其他所有线程执行完某件事后再执行。
  * 例子：解析一个Excel里多个sheet的数据时，可以考虑使用多线程，每个线程解析一个sheet里的数据，等到所有的sheet都解析完之后，
- * 程序需要提示解析完成。在这个需求中，要实现主线程等待所有线程完成sheet的解析操作
+ * 程序需要提示解析完成。在这个需求中，要实现主线程等待所有线程完成sheet的解析操作，也可以用join方法来实现
  * @author wangjianlou 2018年8月22日
  * @version V1.0
  */
